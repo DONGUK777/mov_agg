d600a80db59cdce45e9155ef841ba27a129b3e1b
DONGUK777 <kimdongyork@gmail.com>
2024-08-01 18:17:48 +0900

    suumary

--- src/mov_agg/u.py
+++ src/mov_agg/u.py
@@ -12,7 +12,7 @@ def merge(load_dt="20240724"):
        ]
     df = read_df[cols]
     # 울버린만 조회
-    df_where = df[df['movieCd'] == '20247781'].copy() 
+    df_where = df[(df['movieCd'] == '20247781') & (df['load_dt'] == int(load_dt))].copy()
     print(df_where)
     print(df_where.dtypes)
 
@@ -21,6 +21,20 @@ def merge(load_dt="20240724"):
     df_where['multiMovieYn'] = df_where['multiMovieYn'].astype('object')
     df_where['repNationCd'] = df_where['repNationCd'].astype('object')
     print(df_where.dtypes)
-    return df_where
+    
+    # NaN 값 unknown으로 변경
+    df_where['multiMovieYn'] = df_where['multiMovieYn'].fillna('unknown')
+    df_where['repNationCd'] = df_where['repNationCd'].fillna('unknown')
+    print(df_where.dtypes)
+    print(df_where)
+
+    #머지
+    u_multi = df_where[df_where['multiMovieYn'] == 'unknown']
+    u_nation = df_where[df_where['repNationCd'] == 'unknown']
+    m_df = pd.merge(u_multi, u_nation, on='movieCd', suffixes=('_m', '_n'))
+
+    print("머지 DF")
+    print(m_df)
+    return m_df
 
 merge()
